7185728f3b29d213fc186f3a2f6c7568d239abdc
Sameer jaga <lalit.03@icloud.com>
2025-02-02 10:02:30 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,22 @@
+"use strict ";  //treated like a newer viersion code of java script//
+// alert(3+4)// we are using node js no browser
+console.log(9+
+    3)// code always readable
+
+console.log("sameer")
+let accno=34
+let id="sameer jaga "
+let isidlooged= true
+let sameer ;
+let isd = null// it is object 
+/* datatype in java script
+//number => 2 power to 53 
+//big int 
+// string =>""
+// boolean => true /flase
+// null=>standalone 
+// undefined => defing a variable only not value
+// symbol= for unique (use in react js for elemnet)
+// object
+*/
+console.table([typeof accno, id, isidlooged,sameer,isd]);
