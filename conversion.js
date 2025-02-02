5fdd15ae9ab4b8f83fde88b1d9e5fb949cf1ce4a
Sameer jaga <lalit.03@icloud.com>
2025-02-02 11:53:07 +0000

    conversion and opretion

---
+++ conversion.js
@@ -0,0 +1,15 @@
+let score= 33
+ console.log(typeof (score))
+ let vlaue = Number(score);
+ console.log( typeof vlaue);
+ console.log(vlaue) 
+ 
+// "33" => 33
+//  "33ab" +> nan
+//true => 1;
+// flase=>0;
+let isloogedin =Boolean(score)
+console.log(isloogedin)
+let vari = String(score)
+console.log(vari)
+console.log(typeof (vari))
