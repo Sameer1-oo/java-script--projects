58a7396e538ab83d88bfbf266cfc7aa4fe26598f
Sameer jaga <lalit.03@icloud.com>
2025-02-02 07:07:20 +0000

    variable

---
+++ variable.js
@@ -0,0 +1,11 @@
+  const account_Id=14453
+  let account_email = "sameerjaga23232@Gmail.com"
+  var account_password = "12323"
+  accountcity="jaipur"
+   let accountstate;
+//   account_Id = 2// not allowed
+account_email="samsaeee@gmail.com"
+  console.log(account_Id);
+  console.table([account_Id, account_email, account_password, accountcity,accountstate])
+  /* prefere not to use to var  beacuse of use issue in block scop{}, and functional scope 
+  */
