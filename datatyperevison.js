213afd13fb97fe267b79744cf5a886f57b1a3018
Sameer jaga <lalit.03@icloud.com>
2025-02-05 11:41:43 +0000

    data type reviosn

---
+++ datatyperevison.js
@@ -0,0 +1,29 @@
+// primitive data type 
+/* 7 type : string , number , boolean , null , undefind, symbol, big int
+*/
+// java script is dynamical language  it has not to worry when datais no inform 
+const scroe = 100
+const isloggedin= false 
+const outsidetemp= null
+let userEmail;
+
+// refrence data type (non prumetive data type )
+// array , obejects, function
+const id = Symbol('1234')
+const anthoreid = Symbol("1234")
+console.log(id== anthoreid)
+const heros = ["shaktiman, nagaraj,doga"] 
+// refrence datatype 
+let myobj=
+{
+    name : "sameer"
+
+    // object in curly bracket 
+}
+const  myfuncation = function(){
+    console.log("hello world");
+}
+console.log(typeof outsidetemp)
+console.log(myfuncation)
+console.log(typeof myfuncation)
+// always refernce datatype of typeof is object
