731180f94a11582c289cfcc246e91031e0064759
Sameer jaga <lalit.03@icloud.com>
2025-02-05 11:58:38 +0000

    string and number and prefix and postfix

--- conversion.js
+++ conversion.js
@@ -13,3 +13,24 @@ console.log(isloogedin)
 let vari = String(score)
 console.log(vari)
 console.log(typeof (vari))
+/* ****************opretion************************/
+//  basics opretion 
+console.log(2+2)
+console.log(2*2)
+console.log(2-2)
+console.log(2%2)
+//  reminder 
+console.log(2/2)
+console.log(2**2)
+// for power opretion 3^3
+// you can add two string toghter
+let str1 = "sameer very"
+let str2 = "jaaga"
+let str3= str1+str2
+console.log(str3)
+// 
+let str4 = "same"
+let str5 = 9
+let str6 = str4+str5
+console.log(typeof str6)
+
---
+++ string-num.js
@@ -0,0 +1,21 @@
+let a = 09
+//  zero leading decimal number not allowed "let a = 9"
+let d = "sameer "
+let e = a+d
+console.log(e)
+console.log(typeof e)
+// e is string 
+// 9sameer
+// problem metic case
+console.log(1+2)
+console.log("1"+2)
+console.log(1+"3")
+console.log("1"+2+2)
+console.log(1+"2"+2)
+// output
+// 3
+// 12
+// 13
+// 122
+// 32 but  dirrent value why. this is rule of java script read in ecma script
+let gamecounter =
