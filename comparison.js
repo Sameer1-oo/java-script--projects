7212ec93eb9eb34c504a3d17659cdcb0fbff5472
Sameer jaga <lalit.03@icloud.com>
2025-02-05 11:01:34 +0000

    comparison

---
+++ comparison.js
@@ -0,0 +1,20 @@
+console.log(2>1)
+console.log(2<1)
+console.log(3==2)
+// /* basic comprison */
+console.log("2"<1)
+console.log("2">1)
+// in this case code run but it is a bad method ensure that variable 
+// must be same 
+console.log(null < 0)
+console.log(null>=0)
+console.log(null >0)
+/* thw reason that is an equality chek == and comparisons <= and 
+>= work diffrently 
+comparison convert null to number treating it as a 0 that why null
+<=0 that case 
+*/
+console.log(undefined = 0)
+//  there double eqaul own a mean and triple equal own a anthore mean 
+// === strictly cheked the comparisio test and chek also data type .
+// == in this case having conversion test 
