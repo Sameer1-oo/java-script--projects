90093d9ea277c4e8b213aa3f1332780cb920928f
Sameer jaga <lalit.03@icloud.com>
2025-02-06 15:54:12 +0000

    dates and depth

---
+++ dates.js
@@ -0,0 +1,13 @@
+// dates
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleTimeString())
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+//  we can decler the dates varours method like the ⬆️⬆️
+//  but how we can decler a sfcipic date
+let mycreate = new Date(2025,0 ,2);
+console.log(mycreate);
+console.log(mycreate.toDateString());
