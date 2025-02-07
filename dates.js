9d69a7d66c029546d4435f2b66fbcf640b6983a6
Sameer jaga <lalit.03@icloud.com>
2025-02-07 06:52:42 +0000

    dates and time

--- dates.js
+++ dates.js
@@ -8,6 +8,29 @@ console.log(mydate.toLocaleString());
 console.log(typeof mydate);
 //  we can decler the dates varours method like the ⬆️⬆️
 //  but how we can decler a sfcipic date
-let mycreate = new Date(2025,0 ,2);
+let mycreate = new Date(2025,0 ,2 , 2 , 3);
 console.log(mycreate);
 console.log(mycreate.toDateString());
+console.log(mycreate.toLocaleDateString());
+//  in this java script the month will be start with zero .
+//  how you difne the timme
+// many formate to delcere time and date more pericise
+let myTimestamp =  Date.now()
+console.log(myTimestamp)
+//  calucute the time stamp
+//  comapring the time stamp beetween to date likke "console.log(myTimestamp.getTime())"
+console.log(mycreate.getTime());
+console.log(mycreate.getMilliseconds());
+console.log(Date.now()/1000);
+//  that input give mili second output but in decimle  how to reduce thet thing 
+// we are using Math.floor funaction to reduce decimal value exact 
+console.log(Math.floor(Date.now()/1000))
+let newdate =  new Date()
+console.log(newdate)
+console.log(newdate.getDay()+1)
+//  there we are use + 1 beascues in java ascript months start with 0  
+console.log(newdate.getMonth());
+newdate.toLocaleString('defalut',{
+    weekday: "long"
+}) 
+//  this method ⬆️⬆️ is very good use when you want use it
