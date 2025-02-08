598380c75471e5834145c5b479fa7c3af4cdf8e9
Sameer jaga <lalit.03@icloud.com>
2025-02-08 10:55:25 +0000

    in depth  objects  important topics

--- objects.js
+++ objects.js
@@ -25,20 +25,84 @@ const user ={
  // singleton method constructive 
  
 //   when you to change
- console.log(user)
-console.log(user["email"])
+//  console.log/(user)
+// console.log(user["email"])
 //  right way to exis the object 
-console.log(user[mysymbol])
-console.log(typeof user[mysymbol])
+// console.log(user[mysymbol])
+// console.log(typeof user[mysymbol])
 
 //  we can freez the value of object and nobody can't change the value 
 
 //Object.freeze(user)
 user.email='sameerjaga';
-console.log(user);
+// console.log(user);
 user.greetings = function(){
     console.log(`hello sameer jaga ${this.name}` );
 }
-console.log(user.greetings())
+// console.log(user.greetings())
 //  out put of this undifinf
 
+/* ************************** objects-2*******************************/
+
+
+//  we study singleton construt
+
+const tinderuser = new Object()
+tinderuser.id ='sameer_jaga0'
+tinderuser.name='samey'
+tinderuser.isloggedin = false
+
+
+// console.log(tinderuser)
+// output = {}
+
+//  same out put bconst tinderuser = {}ut this is non singloton 
+const regulaeruser = {
+    eamil:'sameerapple@.com',
+    fullname:{
+        userfullname : {
+            fristname: "sameer",
+            lastname : 'jaga '
+        }
+    }
+}
+console.log(regulaeruser.fullname.userfullname.fristname);
+
+//  the nestet object we can define 
+//  optional chaninig
+const obj1 = {
+    1:'a',2:'b'
+}
+const obj2 ={
+    3:'c',4:'d'
+}
+//  two object can be mreger 
+const merged1 = Object.assign({}, obj1, obj2);
+console.log(merged1)
+//  we can store our object in arryas like this 
+const mer = [
+    {
+        id: '123abc',
+        email:'samer@gmail.com'
+
+    }, {
+        id: '123abc',
+        email:'samer@gmail.com'
+
+    }, {
+        id: '123abc',
+        email:'samer@gmail.com'
+
+    }
+
+]
+mer[1].id
+console.log(mer)
+console.log(tinderuser)
+console.log(Object.keys(tinderuser))
+// this snatxa this pur the all key in tinderuser  it conver into arrays
+//  and also we can extract values 
+console.log(Object.values(tinderuser))
+console.log(Object.entries(tinderuser))
+ // we can checked the value has or not 
+ console.log(tinderuser.hasOwnProperty("isloggedin"))
