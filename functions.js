0c6272162824278be9cc8b53814b35af2c38e0ef
Sameer jaga <lalit.03@icloud.com>
2025-02-10 08:33:10 +0000

    finctions  important  topics

---
+++ functions.js
@@ -0,0 +1,42 @@
+
+//  right santax of function code in java script
+function myname (){
+    //  wriet the defination of work 
+    console.log('s')
+console.log('s')
+console.log('s')
+
+}
+
+ myname()
+//   this is refernce call  functions
+//  this is right way to ecute the function 
+// function addtwonumbers(number1 ,number2){
+//     console.log(number1+number2)
+// }
+// addtwonumbers(3,4)
+//  this are argute 3,4
+//  number1 and number 2 is preameter
+
+//  how to identify the two elements number or not a number 
+//  anthor method to declear 
+function addtwonumbers(number1 ,number2){
+    // let result = number1+number2
+    return number1+number2
+    //  after the return  any function dosn't work 
+}
+const result= addtwonumbers(23,873636)
+console.log('result:' ,result)
+function loginusermessage(username){
+    if(username === undefined){
+console.log('please enter a username ');
+return 
+//  this the santax to write if and else function
+
+    }
+    return`${username} user just logedin`
+}
+ console.log(loginusermessage('sameer'))
+//   decelaertion of variabel there store value
+//  when we ar not give the value in the brackets  out come with undefined
+
