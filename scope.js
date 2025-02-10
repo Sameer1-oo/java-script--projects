fb68b55d9298764fc7ce10f4f5bd7d95922034da
Sameer jaga <lalit.03@icloud.com>
2025-02-10 09:47:12 +0000

    scop block  and global scop

---
+++ scope.js
@@ -0,0 +1,20 @@
+//  let , var  and const
+
+//  they are work good but where it is use scope in problem in {}
+if (true) {
+     let a =20
+    const b = 10
+    var c= 30
+}  /* in this { } brakects block scop
+and out side globle scop
+block scop ki value bhar nhi jani chiye simple si baat 
+*/
+
+//  var does not work with var ***
+// {} in this we are use many varible but var is not good for  scope
+
+
+
+// console.log(a)
+// console.log(b)
+console.log(c)
