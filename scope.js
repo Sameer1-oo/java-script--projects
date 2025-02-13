3c71fb379490b5302d4dadb2b035c4689fbd0c99
Sameer jaga <lalit.03@icloud.com>
2025-02-13 06:35:07 +0000

    nested scope , gobal scope and nested scope

--- scope.js
+++ scope.js
@@ -17,4 +17,44 @@ block scop ki value bhar nhi jani chiye simple si baat
 
 // console.log(a)
 // console.log(b)
-console.log(c)
+// console.log(c)
+/* nestesd scop */
+
+function one (){
+    const username = 'sameer'
+    function two (){
+        const website = 'youtube'
+        // console.log(username);
+    }
+    // console.log(website);
+    // this is not run beacuse this two function is a block scop for global scop 
+    two()
+}
+one()
+ if(/*condition*/ true) {
+    const username = 'sameerjaga'
+    if (username ==='sameerjaga') {
+        const website ="youtube"
+        console.log(username + website);       
+    }
+    // console.log(website)
+ }
+//  console.log(username)
+
+
+// +++++++++++++++++++++++++ intersting ++++++++++++++++
+
+
+
+function addone(num){
+    return num +1
+
+}
+addone(6)
+
+
+const addtwo =function(num)
+{
+    return num + 2
+}
+addtwo(7)
