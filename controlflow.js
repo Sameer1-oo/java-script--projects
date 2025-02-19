7ec96f51eede88201bc07c384cafa26078cbe39e
Sameer jaga <lalit.03@icloud.com>
2025-02-19 12:34:13 +0000

    control flow   and comprision method

---
+++ controlflow.js
@@ -0,0 +1,44 @@
+// // if statmnernt
+// const isusserlogedin = true
+// if(/*condtion*/ 2==="2"){
+//     console.log(true)
+//     //  { } block scope
+// } else {
+//     console.log(false)
+// }
+// //  conditional statmenrt <, >, <=,>=,==, and ===
+// // = this is a ssinged value opreter
+
+// const score =200
+// if (score >100) {
+//     // const power ='fly'
+//     //  when we aare use var as rplece then const this varible work out side the scope block global scople me run karta hai 
+
+//     var power = "fly"
+//     console.log(`user power ${power}`);
+    
+// }     console.log(`user power ${power}`);
+    
+ const balance = 1000
+//  if (balance >500) console.log('test'), console.log('test2'); don't do that immaturr code
+    //  this is a method to declere  without scope block this is implecit function 
+//  maan lo scope hai only in one line me likh skate hai 
+    
+//  multiple condtional we can check
+// if (balance<500) {
+//     console.log('less than 500')
+    
+// } else if (balance <750) {
+//     console.log('this less than 750 ')
+    
+// } 
+// // that is method to  exicution of multiple condition many consditon we can  
+
+const userloggedin = true
+const debitcard = true
+if (userloggedin && debitcard) {
+    console.log('allow to buy course')
+    
+} else {
+    
+}
