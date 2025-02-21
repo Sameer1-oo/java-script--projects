2e57859cbf143c3ab8e1bbbaadd1d58007b7e251
Sameer jaga <lalit.03@icloud.com>
2025-02-21 11:25:15 +0000

    switch concept and  this is use for multiple condtion exicution

---
+++ switch.js
@@ -0,0 +1,26 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month =3
+switch (month){
+    case 1 :
+        console.log('jannury ')
+         break;
+         case 2 :
+            console.log('feb')
+             break;
+             case 3 :
+                console.log('march ')
+                 break; // it help to stop exicution of unwanted code 
+                 case 1 :
+                    console.log('april')
+                     break;
+                     default:// this is working like  else no condtion and hlep to stop code 
+                        console.log('case dose not match ')
+                        break ;
+}
