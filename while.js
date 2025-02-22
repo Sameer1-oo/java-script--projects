d91b1536a6c1cae36f8924c4c8a8d101a646dcdc
Sameer jaga <lalit.03@icloud.com>
2025-02-22 11:47:01 +0000

    while and do-while loop concept

---
+++ while.js
@@ -0,0 +1,21 @@
+//  let index=0
+//  while (index <=10) {
+//     console.log(`value of is ${index}`);
+//     index = index+2
+    
+// }
+
+
+
+// let myarray = ['flash',' batman','ironman']
+// let arr = 0
+// while (arr <myarray.length){
+//     console.log(`value is ${myarray[arr]}`);
+//     arr++
+// }
+
+let score =1 
+do {console.log(`score is ${score}`)
+score ++
+    
+} while (score <9);
